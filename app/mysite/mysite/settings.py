904c1f456ecf993e8a01afd25b7b32cfc4f800bc
Dennis Sanchez <dennisrsanchez@gmail.com>
2022-03-31 20:58:54 +0000

    allowed hosts

--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q5^3azy)j0a-v5pq#lxps+kq68v88jgxlyxz^x3vs5fdwi$$45
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
